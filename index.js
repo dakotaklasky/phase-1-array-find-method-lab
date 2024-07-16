ad6a4f67e942d963dac3fca9107876d7935e1ba9
Dakota <dakota.klasky@gmail.com>
2024-07-16 17:00:37 -0400

    create function

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+
+function superbowlWin(myArray){
+    const win = myArray.find( (element) => element.result === "W")
+    if(win === undefined){
+        return undefined
+    }
+
+    else{
+        return win.year
+    }
+
+
+}
+
+
+
